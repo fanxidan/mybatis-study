0f204787e49aeff2a36af7157ffa90146b657943
范曦丹 <fanxidan@outlook.com>
2021-06-03 20:30:30 +0800

    mybatis study

---
+++ mybatis-01/src/test/java/com/kuang/dao/UserDaoTest.java
@@ -0,0 +1,28 @@
+package com.kuang.dao;
+
+import com.kuang.pojo.User;
+import com.kuang.utils.MybatisUtils;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+
+import java.util.List;
+
+public class UserDaoTest {
+    @Test
+    public void test(){
+        //获取sqlSession对象
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        //执行sql
+        //方式1：getMapper
+        UserDao mapper = sqlSession.getMapper(UserDao.class);//根据接口的类名绑定到xml中
+        List<User> userList1 = mapper.getUserList();
+
+        //方式2
+        List<User> userList2 = sqlSession.selectList("com.kuang.dao.UserDao.getUserList");
+
+        for (User user : userList1) {
+            System.out.println(user);
+        }
+        sqlSession.close();
+    }
+}
